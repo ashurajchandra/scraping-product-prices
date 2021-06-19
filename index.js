a765aea05c92c0790bea296f5091d7f5fe6b33e7
Ashutosh rai <67899702+ashurajchandra@users.noreply.github.com>
2021-06-19 21:47:45 +0530

    updated code

--- index.js
+++ index.js
@@ -3,41 +3,63 @@ const $ = require('cheerio');
 const CronJob = require('cron').CronJob;
 const nodemailer = require('nodemailer');
 
-const url = 'https://www.amazon.in/Dell-Latitude-3410-Windows10Pro-Fingerprint/dp/B08LKGTT23/';
+const url = 'https://www.amazon.com/Sony-Noise-Cancelling-Headphones-WH1000XM3/dp/B07G4MNFS1/';
 
-puppeteer.launch({
-    headless:false
-}).then(async browser => {
+async function configureBrowser() {
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-  /* going to particular url */
-    await page.goto(url); //priceblock_ourprice
-   await  page.waitForSelector('#priceblock_ourprice')
-   
-   /* fetching the total count of corona cases */
-        const price = await page.evaluate(()=>{
-        //    const list=  document.querySelectorAll('#priceblock_ourprice').innerText;
-        const list=  document.querySelector('#priceblock_ourprice').innerHTML
-        let currentPrice = Number(list.replace(/[^0-9.-]+/g,""));
-            //  return list.replace('&nbsp;','').slice(1,10);
-         
-            return currentPrice;
-        });
-        if(price <= 94000){
-            console.log("WOW! IT'S TIME TO BUY", price)
-        }
-      
-        console.log("price",price)
-        async function startTracking() {
-            const page = await configureBrowser();
-          
-            let job = new CronJob('* */30 * * * *', function() { //runs every 30 minutes in this config
-              price();
-            }, null, true, null, null, true);
-            job.start();
-        }
+    await page.goto(url);
+    return page;
+}
 
-      
+async function checkPrice(page) {
+    await page.reload();
+    let html = await page.evaluate(() => document.body.innerHTML);
+    // console.log(html);
 
-         /* closing browser after completion */
-        // await browser.close()
-})
+    $('#priceblock_dealprice', html).each(function() {
+        let dollarPrice = $(this).text();
+        // console.log(dollarPrice);
+        let currentPrice = Number(dollarPrice.replace(/[^0-9.-]+/g,""));
+
+        if (currentPrice < 300) {
+            console.log("BUY!!!! " + currentPrice);
+            sendNotification(currentPrice);
+        }
+    });
+}
+
+async function startTracking() {
+    const page = await configureBrowser();
+  
+    let job = new CronJob('* */30 * * * *', function() { //runs every 30 minutes in this config
+      checkPrice(page);
+    }, null, true, null, null, true);
+    job.start();
+}
+
+async function sendNotification(price) {
+
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: '*****@gmail.com',
+        pass: '*****'
+      }
+    });
+  
+    let textToSend = 'Price dropped to ' + price;
+    let htmlText = `<a href=\"${url}\">Link</a>`;
+  
+    let info = await transporter.sendMail({
+      from: '"Price Tracker" <*****@gmail.com>',
+      to: "*****@gmail.com",
+      subject: 'Price dropped to ' + price, 
+      text: textToSend,
+      html: htmlText
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+  }
+
+startTracking();
